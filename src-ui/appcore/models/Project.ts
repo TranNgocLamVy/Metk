474b4eb08dbe5e81ae2baa39512e1bf536b34993
Tran Ngoc Lam Vy <vy.tranngoclam@gmail.com>
2025-08-06 22:18:02 +0700

    Improve Project class structure

--- src-ui/appcore/models/Project.ts
+++ src-ui/appcore/models/Project.ts
@@ -1,20 +1,80 @@
-import { IProject } from "../interfaces/project";
+import EventEmitter from "eventemitter3";
+import { type } from "arktype"
 
+export const ProjectDataParser = type({
+    id: "string",
+    name: "string",
+    version: "string = '0.1.0'",
+    description: "string = ''",
+    createdAt: "Date",
+    updatedAt: "Date",
+    tilemapPath: "string[]",
+    tilesetPath: "string[]",
+})
 
-export class Project implements IProject {
-    public id: string;
+export type ProjectFileData = typeof ProjectDataParser.infer;
+
+export class Project extends EventEmitter {
+    public readonly id: string;
     public name: string;
     public version: string;
     public description: string;
     public createdAt: Date;
     public updatedAt: Date;
+    public tilemap: any[]; // TODO: Tilemap
+    public tileset: any[]; // TODO: Tileset
 
-    constructor(data: IProject) {
+    private constructor(data: ProjectFileData) {
+        super();
         this.id = data.id;
         this.name = data.name;
         this.version = data.version;
         this.description = data.description;
         this.createdAt = data.createdAt;
         this.updatedAt = data.updatedAt;
+
+        data.tilemapPath.forEach((path) => {
+            // TODO: Load tilemap
+        });
+
+        data.tilesetPath.forEach((path) => {
+            // TODO: Load tileset
+        });
+    }
+
+    public serialize(): ProjectFileData {
+        return {
+            id: this.id,
+            name: this.name,
+            version: this.version,
+            description: this.description,
+            createdAt: this.createdAt,
+            updatedAt: this.updatedAt,
+            tilemapPath: this.tilemap.map((tilemap) => {
+                // TODO: Serialize tilemap
+                return "";
+            }),
+            tilesetPath: this.tileset.map((tileset) => {
+                // TODO: Serialize tileset
+                return "";
+            }),
+        };
+    }
+
+    public addTilemap(tilemap: any) {
+        // TODO: Add tilemap
+    }
+
+    public addTileset(tileset: any) {
+
+    }
+
+    static createProject(data: string): Project | null {
+        const projectData = ProjectDataParser(data);
+        if (projectData instanceof type.errors) {
+            console.error(projectData);
+            return null;
+        }
+        return new Project(projectData);
     }
 }
